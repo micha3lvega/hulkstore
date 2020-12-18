5e4def66e2d9f94de5186984cffbcab0aefd1112
Michael Vega <micha3lvega@gmail.com>
2020-12-17 21:30:29 -0500

    Crear proyecto para la administracion de usuarios

---
+++ hulk-store-users/hulk-store-users-services/src/main/java/co/com/hulk/store/services/HulkStoreUsersServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.hulk.store.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulkStoreUsersServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulkStoreUsersServicesApplication.class, args);
+	}
+
+}
---
+++ hulk-store-users/hulk-store-users-services/src/test/java/co/com/hulk/store/services/HulkStoreUsersServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.hulk.store.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkStoreUsersServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
