e09f33b2f1e5b51ae29f7ed628ba13991ed009a9
Michael Vega <micha3lvega@gmail.com>
2020-12-18 01:18:55 -0500

    crear beans de configuracion

---
+++ hulk-store-users/hulk-store-users-services/src/main/java/co/com/hulk/store/services/config/HulkStoreConfig.java
@@ -0,0 +1,22 @@
+package co.com.hulk.store.services.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class HulkStoreConfig {
+
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+	    return new BCryptPasswordEncoder();
+	}
+	
+}
