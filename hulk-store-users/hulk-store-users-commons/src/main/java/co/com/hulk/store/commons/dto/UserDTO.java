c0f12c6849aad01545d9b35be6a4d461cc552e4e
Michael Vega <micha3lvega@gmail.com>
2020-12-18 23:59:48 -0500

    Ajuste de propiedades de validacion

--- hulk-store-users/hulk-store-users-commons/src/main/java/co/com/hulk/store/commons/dto/UserDTO.java
+++ hulk-store-users/hulk-store-users-commons/src/main/java/co/com/hulk/store/commons/dto/UserDTO.java
@@ -3,6 +3,11 @@ package co.com.hulk.store.commons.dto;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
@@ -18,17 +23,31 @@ public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 4465809113751598045L;
 
 	private String id;
-
+	
+	@NotBlank(message = "El nombre es obligatorio")
+	@Size(min = 3, max = 20, message = "El nombre tiene que tener entre 3 y 20 caracteres")
 	private String name;
+
+	@NotBlank(message = "El apellido es obligatorio")
+	@Size(min = 3, max = 20, message = "El apellido tiene que tener entre 3 y 20 caracteres")
 	private String lastName;
 
+	@Email
+	@NotBlank(message = "El correo es obligatorio")
+	@Size(min = 3, max = 20, message = "El correo tiene que tener entre 3 y 20 caracteres")
 	private String email;
+
+	@NotBlank(message = "El telefono es obligatorio")
+	@Size(min = 3, max = 10, message = "El telefono tiene que tener entre 3 y 10 digitos")
 	private String mobile;
+	
+	@NotEmpty(message = "La contraseña es obligatoria")
+	@Size(min = 6, max = 50, message = "El contraseña tiene que tener entre 6 y 50 digitos")
 	private String password;
 
-	private UserStateDTO state;
-	
 	private Date createDate;
+	
 	private Date lastUpdate;
-
+	
+	private UserStateDTO state;
 }
--- hulk-store-users/hulk-store-users-services/src/main/java/co/com/hulk/store/services/model/User.java
+++ hulk-store-users/hulk-store-users-services/src/main/java/co/com/hulk/store/services/model/User.java
@@ -3,11 +3,6 @@ package co.com.hulk.store.services.model;
 import java.util.Date;
 import java.io.Serializable;
 
-import javax.validation.constraints.Size;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.NotEmpty;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -24,34 +19,22 @@ public class User implements Serializable {
 	@Id
 	private String id;
 
-	@NotEmpty(message = "El nombre es obligatorio")
-	@Size(min = 3, max = 20, message = "El nombre tiene que tener entre 3 y 20 caracteres")
 	private String name;
 
-	@NotEmpty(message = "El apellido es obligatorio")
-	@Size(min = 3, max = 20, message = "El apellido tiene que tener entre 3 y 20 caracteres")
 	private String lastName;
 
-	@Email
-	@NotEmpty(message = "El correo es obligatorio")
-	@Pattern(regexp = "[A-Z0-9]+", message="El correo es invalido")
-	@Size(min = 3, max = 20, message = "El correo tiene que tener entre 3 y 20 caracteres")
 	private String email;
 
-	@NotEmpty(message = "El telefono es obligatorio")
-	@Size(min = 3, max = 10, message = "El telefono tiene que tener entre 3 y 10 digitos")
 	private String mobile;
-	
-	@NotEmpty(message = "La contraseña es obligatoria")
-	@Size(min = 6, max = 50, message = "El contraseña tiene que tener entre 6 y 50 digitos")
+
 	private String password;
 
 	@CreatedDate
 	private Date createDate;
-	
+
 	@LastModifiedDate
 	private Date lastUpdate;
-	
+
 	private UserState state;
 
 }
