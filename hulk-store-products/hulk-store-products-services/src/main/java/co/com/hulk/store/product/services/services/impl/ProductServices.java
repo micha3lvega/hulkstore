f6a4dc252fcc0d46daaca72c74d92e43938a1057
Michael Vega <micha3lvega@gmail.com>
2020-12-20 15:44:14 -0500

    Validar que no se puede repetir el nombre del producto al actualizar

--- hulk-store-products/hulk-store-products-services/src/main/java/co/com/hulk/store/product/services/services/impl/ProductServices.java
+++ hulk-store-products/hulk-store-products-services/src/main/java/co/com/hulk/store/product/services/services/impl/ProductServices.java
@@ -38,7 +38,8 @@ public class ProductServices implements IProductServices {
 	@Transactional(readOnly = true)
 	public ProductDTO findById(String id) throws ProductException {
 
-		Product product = repository.findById(id).orElseThrow(() -> new ProductException(ProductCodeException.PRODUCT_NO_EXISTS));
+		Product product = repository.findById(id)
+				.orElseThrow(() -> new ProductException(ProductCodeException.PRODUCT_NO_EXISTS));
 
 		if (product == null) {
 			return null;
@@ -50,22 +51,22 @@ public class ProductServices implements IProductServices {
 	@Override
 	@Transactional
 	public ProductDTO create(ProductDTO dto) throws ProductException {
-		
-		//Normalizar el nombre
+
+		// Normalizar el nombre
 		dto.setName(Util.capitalizeString(dto.getName()));
-		
-		//Buscar si existe el producto
+
+		// Buscar si existe el producto
 		Product findProduct = repository.findByName(dto.getName());
-		
+
 		if (findProduct != null) {
 			throw new ProductException(ProductCodeException.PRODUCT_ALREADY_EXISTS);
 		}
-		
+
 		// Asignar fecha de ingreso si es nula
 		if (dto.getEntryDate() == null) {
 			dto.setEntryDate(new Date());
 		}
-				
+
 		Product product = mapper.map(dto, Product.class);
 		product = repository.insert(product);
 
@@ -76,6 +77,17 @@ public class ProductServices implements IProductServices {
 	@Override
 	@Transactional
 	public ProductDTO update(ProductDTO dto) throws ProductException {
+
+		// Normalizar el nombre
+		dto.setName(Util.capitalizeString(dto.getName()));
+
+		// Buscar si existe el producto
+		Product findProduct = repository.findByName(dto.getName());
+
+		// Arrojar una excepcion si ya existe otro producto con ese nombre
+		if (findProduct != null && !dto.getName().equals(findProduct.getName())) {
+			throw new ProductException(ProductCodeException.PRODUCT_ALREADY_EXISTS);
+		}
 
 		Product product = mapper.map(dto, Product.class);
 		product = repository.save(product);
