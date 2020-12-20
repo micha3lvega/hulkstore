bfeafeb014cf5d6a91dba5be0a2c3e33e64ca2f0
Michael Vega <micha3lvega@gmail.com>
2020-12-20 16:08:04 -0500

    Creacion del proyecto para la administracion del carrito de compras

---
+++ hulk-store-shopping-cart/hulk-store-shopping-cart-services/src/main/java/co/com/hulk/store/shopping/cart/services/HulkStoreShoppingCartCommonsServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.hulk.store.shopping.cart.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulkStoreShoppingCartCommonsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulkStoreShoppingCartCommonsServicesApplication.class, args);
+	}
+
+}
---
+++ hulk-store-shopping-cart/hulk-store-shopping-cart-services/src/test/java/co/com/hulk/store/shopping/cart/services/HulkStoreShoppingCartCommonsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.hulk.store.shopping.cart.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkStoreShoppingCartCommonsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
