089e1f092198b5221c807759b925e38720900c5b
Michael Vega <micha3lvega@gmail.com>
2020-12-20 16:22:48 -0500

    Creacion del proyecto para la administracion de clientes

---
+++ hulk-store-customer/hulk-store-customer-services/src/main/java/co/com/hulk/store/customer/services/HulkStoreCustomerServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.hulk.store.customer.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulkStoreCustomerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulkStoreCustomerServicesApplication.class, args);
+	}
+
+}
---
+++ hulk-store-customer/hulk-store-customer-services/src/test/java/co/com/hulk/store/customer/services/HulkStoreCustomerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.hulk.store.customer.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkStoreCustomerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
