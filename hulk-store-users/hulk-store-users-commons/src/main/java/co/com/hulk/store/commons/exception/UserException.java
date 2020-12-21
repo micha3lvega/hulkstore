cb9bf9bbfe259d17e7755ca6b0ba109e6a088e52
Michael Vega <micha3lvega@gmail.com>
2020-12-21 10:29:10 -0500

    Eliminar codigo de error en las excepciones de usuarios

--- hulk-store-users/hulk-store-users-commons/src/main/java/co/com/hulk/store/commons/exception/UserException.java
+++ hulk-store-users/hulk-store-users-commons/src/main/java/co/com/hulk/store/commons/exception/UserException.java
@@ -5,7 +5,7 @@ public class UserException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public UserException(UserExceptionCode exceptionCode) {
-		super(exceptionCode.getCodigo() + "-" + exceptionCode.getMensaje());
+		super(exceptionCode.getMensaje());
 	}
 
 	public UserException(String mensaje) {
--- hulk-store-users/hulk-store-users-commons/src/main/java/co/com/hulk/store/commons/exception/UserExceptionCode.java
+++ hulk-store-users/hulk-store-users-commons/src/main/java/co/com/hulk/store/commons/exception/UserExceptionCode.java
@@ -2,23 +2,17 @@ package co.com.hulk.store.commons.exception;
 
 public enum UserExceptionCode {
 
-	EMAIL_REPEAT_EXCEPTION("El correo ya existe", "001"), USER_NO_EXITS("El usuario no existe", "002"),
-	LOGIN_ERROR("Usuario o contraseña incorrectos", "003"), INVALID_EMAIL("El correo es innecesario","004");
+	EMAIL_REPEAT_EXCEPTION("El correo ya existe"), USER_NO_EXITS("El usuario no existe"),
+	LOGIN_ERROR("Usuario o contraseña incorrectos"), INVALID_EMAIL("El correo es innecesario");
 
 	private String mensaje;
-	private String codigo;
 
-	private UserExceptionCode(String mensaje, String codigo) {
+	private UserExceptionCode(String mensaje) {
 		this.mensaje = mensaje;
-		this.codigo = codigo;
 	}
 
 	public String getMensaje() {
 		return mensaje;
-	}
-
-	public String getCodigo() {
-		return codigo;
 	}
 
 }
